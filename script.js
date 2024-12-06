819bef4820d2b458cddbf22a022963ac06ffec47
Kenta Hirata <kenta.hirata@gmail.com>
2024-12-07 01:47:11 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// const family_modal = document.getElementById("family_modal");
+
+function modalOpen() {
+    // family_modal.style.display = "block";
+    $("#family_modal").fadeIn();
+}
+
+function modalClose() {
+    // family_modal.style.display = "none";
+    $("#family_modal").fadeOut();
+}
+
