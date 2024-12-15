67ec6f07034050c94023d5120dc4a9f730513925
Kenta Hirata <kenta.hirata@gmail.com>
2024-12-15 17:26:34 +0900

    complete

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
-// const family_modal = document.getElementById("family_modal");
+window.addEventListener('scroll', function(){
+    const scroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const targets = document.querySelectorAll(".fade_item1, .fade_item2, .fade_item3, .fade_item4");
+    const lines1 = this.document.querySelectorAll(".line_before1");
+    const lines2 = this.document.querySelectorAll(".line_before2");
 
-function modalOpen() {
-    // family_modal.style.display = "block";
-    $("#family_modal").fadeIn();
+    // 各モーダルはスクロール時に表示
+    targets.forEach(function(target) {
+        const distanceToTitle = target.offsetTop;
+        if(scroll + windowHeight - 500 > distanceToTitle) {
+            target.classList.add("is-active");
+        }
+    });
+
+    // テキストにラインアニメーション
+    lines1.forEach(function(line) {
+        const distanceToLine = line.offsetTop;
+        if(scroll + windowHeight - 500 > distanceToLine) {
+            line.classList.add("line_after1");
+        }
+    });
+    lines2.forEach(function(line) {
+        const distanceToLine = line.offsetTop;
+        if(scroll + windowHeight - 500 > distanceToLine) {
+            line.classList.add("line_after2");
+        }
+    });
+});
+
+// モーダルはフェードイン表示
+function modalOpen(name) {
+    if(name == family) {
+        $("#family_modal").fadeIn();
+    }
+    else if(name == dog) {
+        $("#dog_modal").fadeIn();
+    }
+    else if(name == beer) {
+        $("#beer_modal").fadeIn();
+    }
+    else if(name == chiikawa) {
+        $("#chiikawa_modal").fadeIn();
+    }
+    else if(name == homeTown) {
+        $("#homeTown_modal").fadeIn();
+    }
+    else if(name == unity) {
+        $("#unity_modal").fadeIn();
+    }
+    else if(name == rubikCube) {
+        $("#rubikCube_modal").fadeIn();
+    }
+    else if(name == ramen) {
+        $("#ramen_modal").fadeIn();
+    }
+    else if(name == book) {
+        $("#book_modal").fadeIn();
+    }
 }
 
+// モーダルはフェードアウトで閉じる
 function modalClose() {
-    // family_modal.style.display = "none";
-    $("#family_modal").fadeOut();
+    $(".modal").fadeOut();
 }
-
